e33cee58bde18c82d0e9f830558796d2716f7ff3
User <user@MacBook-Pro-User.local>
2018-12-02 15:53:53 +0600

    first commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by User on 12/2/18.
+//  Copyright © 2018 Arnur B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
